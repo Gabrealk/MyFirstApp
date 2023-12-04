2b7f96f8fcfbc17baebfb36f519a0964812dc8a9
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-12-03 19:44:08 -0700

    lab 6 API

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import MainLayout from '../layouts/MainLayout';
+import Chuck from '../components/chuck';
 
 function About({ }) {
     return (
@@ -8,6 +9,7 @@ function About({ }) {
             <View>
                 <Text>welcome to Gabreal's To Do List </Text>
                 <Text>To access please go back and add to the todo list!</Text>
+                <Chuck />
             </View>
         </MainLayout>
     );
